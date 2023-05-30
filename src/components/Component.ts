beb2e79b67289d7d686c2bae1c175804bc06d74b
장한울 <swingjhw@naver.com>
2023-05-30 20:11:25 +0900

    feat(Component): 기본 컴포넌트 클래스 생성

---
+++ src/components/Component.ts
@@ -0,0 +1,35 @@
+
+
+export interface IComponent {
+    target: HTMLElement
+    state: Object
+}
+export class Component {
+    #target: HTMLElement
+    #state: Object
+    constructor({target , state}:IComponent){
+        this.#target = target
+        this.#state = state
+
+        this.render()
+        // const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this))
+        // this.bindAllMethods(methods)
+
+    }
+    // bindAllMethods(methods:any[]):void {
+    //     methods.forEach((method:any) => {
+    //         if (method !== 'constructor'){
+    //             this[method]
+    //         }
+    //     })
+    // }
+
+    render():void{
+        this.#target.innerHTML = this.template()
+    }
+
+    template():string{
+        return ''
+    }
+}
+
---
+++ src/components/NavBar.ts
@@ -0,0 +1,10 @@
+import { Component } from "./Component";
+
+
+
+class NavBar extends Component {
+
+    constructor() {
+        
+    }
+}
