c78a9f73f2e9d3a8da357c0c86426539b4a12cac
장한울 <swingjhw@naver.com>
2023-05-30 23:43:50 +0900

    feat: 이미지파일 모두 types.ts에 모듈화

---
+++ src/images/types.ts
@@ -0,0 +1,9 @@
+import Logo from "./logo.png"
+import Search_button from "./search_button.png"
+import Star_empty from "./star_empty.png"
+import Star_filled from "./star_filled.png"
+
+
+
+
+export {Logo, Search_button, Star_empty, Star_filled}
