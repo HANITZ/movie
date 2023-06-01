6611bc551b624ac24bb2e9c6634e44c5de5d52fa
장한울 <swingjhw@naver.com>
2023-06-01 19:44:37 +0900

    feat(HTTPError): HTTPError 양식

---
+++ src/utils/HTTPError.ts
@@ -0,0 +1,11 @@
+
+
+export class HTTPError extends Error {
+    statusCode: number;
+
+    constructor(statusCode: number, message?: string){
+        super(message)
+        this.name = 'HTTPError';
+        this.statusCode = statusCode;
+    }
+}
