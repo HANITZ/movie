d0f075524f27615256e8e3baaf6c6abc558c18ed
장한울 <swingjhw@naver.com>
2023-06-01 19:45:21 +0900

    feat(Movie): 개별 영화 class 생성

---
+++ src/components/Movie.ts
@@ -0,0 +1,39 @@
+import { Star_filled } from "../images/types";
+import { IMovie } from "../types/IMovieListRes";
+
+
+class Movie {
+    #target: HTMLElement
+    #movie: IMovie
+    constructor(target: HTMLElement, movie:IMovie){
+        this.#target = target
+        this.#movie = movie
+        
+        this.render()
+    }
+
+    render():void{
+        this.#target.insertAdjacentHTML("beforeend", this.template())
+    }
+
+    template(): string{
+        const {poster_path, title, vote_average} = this.#movie
+        return `<li>
+        <a href="#">
+          <div class="item-card">
+            <img
+              class="item-thumbnail"
+              src="http://image.tmdb.org/t/p/w300${poster_path}"
+              loading="lazy"
+              alt="${title}"
+            />
+            <p class="item-title">${title}</p>
+            <p class="item-score"><img class="img-star" src="${Star_filled}" alt="별점" /> ${vote_average}</p>
+          </div>
+        </a>
+      </li>`
+    }
+}
+
+
+export default Movie
