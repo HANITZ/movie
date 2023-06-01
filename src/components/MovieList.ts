7b86fc2a097e66ce0672ad9cfe35b5ba118956f2
장한울 <swingjhw@naver.com>
2023-06-01 23:17:04 +0900

    fix: 빈 문자로 검색하는 경우를 고려한 수정

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -14,15 +14,14 @@ class MovieList {
   #target: HTMLElement
   #page: number
   #type: string
-  #searchInput?: string
+  #searchInput: string
 
     constructor(target: HTMLElement, {type, searchInput}:IProps){
       this.#target = target
       this.#page = 1
       this.#type = type
-      if(searchInput){
-        this.#searchInput = searchInput
-      }
+
+      this.#searchInput = searchInput || ''
 
       this.render()
       this.setEvent()
@@ -31,37 +30,48 @@ class MovieList {
     async render(){
       this.#target.innerHTML = this.template()
       
+      
+
       this.renderTitle()
       this.renderMovieList()
     }
 
     async renderMovieList(){
-  
+      
       const ul = $(".item-list", this.#target)
       const fetchedMovies = await this.fetchMovieList()
+      console.log(fetchedMovies)
       if(!fetchedMovies)return
       if(!ul) return
-
+      console.log(fetchedMovies.results.length)
+      if(!this.checkMovies(fetchedMovies)){
+        const searchInput = this.#searchInput
+        this.renderTitle(`"${searchInput}"에 대한 검색결과가 없습니다`)
+        return
+      }
       const {results, total_pages:totalPage} = fetchedMovies
-
+      
       if (!this.checkLastPage(totalPage)) {
         alert('더 이상 페이지가 존재하지 않습니다.')
         return
       }
+
       results.forEach((movie:IMovie) => {
         new Movie(ul ,movie)
       })
       this.#page+=1
     }
-
+    checkMovies(movieList:IMovieListRes){
+      return !!movieList.results.length
+    }
     checkLastPage(totalPage:number):boolean{
       return this.#page < totalPage
     }
 
-    renderTitle(){
+    renderTitle(text?:string):void{
       const title = $('.item-title', this.#target)
       if(!title) return
-      title.innerText = this.#type === 'popular' ? '지금 인기 있는 영화' : `${this.#searchInput} 검색 결과`
+      title.innerText = text || (this.#type === 'popular' ? '지금 인기 있는 영화' : `"${this.#searchInput}" 검색 결과`)
     }
 
     fetchMovieList(){
@@ -69,7 +79,6 @@ class MovieList {
         case 'popular':
           return getPopularMovies(this.#page)
         case 'search':
-          if(!this.#searchInput) return
           return getSearchMovies(this.#page, this.#searchInput)
 
       }
--- src/components/NavBar.ts
+++ src/components/NavBar.ts
@@ -1,33 +1,33 @@
 import { Logo } from "../images/types";
 import { Component } from "./Component";
 
-
-
 interface IState {
-  renderMovieList: (type: string, searchInput?: string) => void
+  renderMovieList: (type: string, searchInput?: string) => void;
 }
 
 class NavBar extends Component {
-  #state
-  constructor(target:HTMLElement, state:IState) {
+  #state;
+  constructor(target: HTMLElement, state: IState) {
     super(target);
-    this.#state=state
+    this.#state = state;
 
     this.setEvent();
   }
 
   setEvent() {
-    const searchBox = this.target.querySelector(".search-box") 
-    const {renderMovieList} = this.#state
-    if (!searchBox) return; 
+    const searchBox = this.target.querySelector(".search-box");
+    const { renderMovieList } = this.#state;
+    if (!searchBox) return;
 
     searchBox.addEventListener("submit", (event) => {
-      event.preventDefault()
-      if(!(event.target instanceof HTMLElement)) return
-      const input = event.target.querySelector('#search-input') as HTMLInputElement
-      if(!input) return;
-      const textValue = input.value.trim()
-      renderMovieList('search', textValue)
+      event.preventDefault();
+      if (!(event.target instanceof HTMLElement)) return;
+      const input = event.target.querySelector(
+        "#search-input"
+      ) as HTMLInputElement;
+      if (!input) return;
+      const textValue = input.value.trim();
+      renderMovieList("search", textValue);
     });
   }
 
